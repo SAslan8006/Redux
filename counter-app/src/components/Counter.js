8113401d8add6b94cff6734f2fc2b9aa5d3c989f
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-18 12:17:15 +0300

    Bir veriyi güncellemek

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, decrementByAmount, increment, incrementByAmount } from '../redux/counter/counterSlice';
 
 function Counter() {
-    const counterValue = useSelector(state => state.counter.value)
+    const [amount, setAmount] = useState(0);
+    const countValue = useSelector(state => state.counter.value)
+    const dispatch = useDispatch();
     return (
-        <div><h1>{counterValue}</h1></div>
+        <div className="container">
+            <h1>Counter</h1>
+            <div>
+                <div className="count-content">
+                    <h1 className="counterNum">{countValue}</h1>
+                </div>
+                <button className="btn btn-increment"
+                    onClick={() => dispatch(decrement())} >
+                    Decrement
+                </button>
+                <button className="btn btn-decrement"
+                    onClick={() => dispatch(increment())} >
+                    Increment
+                </button>
+                <br />
+                <label htmlFor="number" className="count-value">
+                    Amount :
+                </label>
+                <input
+                    type="number"
+                    name="number"
+                    className="nums"
+                    defaultValue={0}
+                    onChange={(event) => setAmount(Number(event.target.value))}
+                />
+                <div>
+                    <button
+                        className="btn btn-inc-by-amount"
+                        onClick={() => dispatch(incrementByAmount(amount))}
+                    >
+                        Increment By Amount
+                    </button>
+                    <button
+                        className="btn btn-inc-by-amount"
+                        onClick={() => dispatch(decrementByAmount(amount))}
+                    >
+                        Decrement By Amount
+                    </button>
+                </div>
+            </div>
+        </div >
     )
 }
 
--- counter-app/src/redux/store.js
+++ counter-app/src/redux/store.js
@@ -1,9 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from "./counter/counterSlice";
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
     },
-
-})
+});
