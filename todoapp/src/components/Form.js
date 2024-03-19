bce52f49258df0476886c612f27cc0635d1925ae
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-19 08:34:28 +0300

    Yeni to-do elemanı eklemek

--- todoapp/src/components/Form.js
+++ todoapp/src/components/Form.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addTodo } from './redux/todos/todoSlice';
+import { nanoid } from "@reduxjs/toolkit"
 function Form() {
+    const [title, setTitle] = useState('')
+    const dispatch = useDispatch()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addTodo({
+            id: nanoid(),
+            title: title,
+            completed: false
+        }))
+        setTitle("");
+    }
     return (
-        <form>
-            <input className='new-todo' placeholder="What needs to be done?" autoFocus />
+        <form onSubmit={handleSubmit}>
+            <input
+                className='new-todo'
+                placeholder="What needs to be done?"
+                autoFocus
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
         </form>
     )
 }
