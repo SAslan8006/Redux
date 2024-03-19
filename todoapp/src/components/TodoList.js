b3e20ee34f2c1f58a87822ab5106547ee62919b1
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-19 13:53:20 +0300

    Filtreleme işlemleri

--- todoapp/src/components/TodoList.js
+++ todoapp/src/components/TodoList.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { toggle, destroy } from './redux/todos/todoSlice';
-
+let filtered = [];
 function TodoList() {
     const dispatch = useDispatch();
-    const items = useSelector((state) => state.todos.items)
+    const { items, activeFilter } = useSelector((state) => state.todos)
+
+
     const handleDestroy = (id) => {
         if (window.confirm('Are you sure?')) {
             dispatch(destroy(id))
         }
     }
+    filtered = items;
+    if (activeFilter !== "all") {
+        filtered = items.filter((todo) =>
+            activeFilter === "active" ?
+                todo.completed === false && todo :
+                todo.completed === true && todo)
+    }
     return (
         <ul className="todo-list">
-            {items.map((item) => (
+            {filtered.map((item) => (
                 <li key={item.id}>
                     <div className={item.completed ? "completed" : "view"}>
                         <input
