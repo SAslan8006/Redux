2219a7fe19a9773cc721eb8e99cbaaf11aaadd53
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-19 10:35:57 +0300

    Silme işlemleri

--- todoapp/src/components/TodoList.js
+++ todoapp/src/components/TodoList.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { toggle } from './redux/todos/todoSlice';
+import { toggle, destroy } from './redux/todos/todoSlice';
 
 function TodoList() {
     const dispatch = useDispatch();
     const items = useSelector((state) => state.todos.items)
+    const handleDestroy = (id) => {
+        if (window.confirm('Are you sure?')) {
+            dispatch(destroy(id))
+        }
+    }
     return (
         <ul className="todo-list">
             {items.map((item) => (
@@ -17,11 +22,12 @@ function TodoList() {
                             onChange={() => dispatch(toggle({ id: item.id }))}
                         />
                         <label>{item.title}</label>
-                        <button className="destroy"></button>
+                        <button className="destroy" onClick={() => handleDestroy(item.id)}></button>
                     </div>
                 </li>
-            ))}
-        </ul>
+            ))
+            }
+        </ul >
     )
 }
 
