8d2ad77cf4d864f7a5eb6cd7ec0ce9d1715a3762
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-21 10:58:19 +0300

    API üzerinden verileri çekmek (Thunk Middleware)

--- todoapp/src/components/TodoList.js
+++ todoapp/src/components/TodoList.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { toggle, destroy, selectFilteredTodos } from './redux/todos/todoSlice';
+import { toggle, destroy, selectFilteredTodos, getTodosAsync } from './redux/todos/todoSlice';
+import Loading from './Loading';
+import Error from './Error';
 function TodoList() {
     const dispatch = useDispatch();
     const filteredItems = useSelector(selectFilteredTodos)
+    const { isLoading, error } = useSelector(state => state.todos)
+    useEffect(() => {
+        dispatch(getTodosAsync());
+    }, [dispatch])
 
     const handleDestroy = (id) => {
         if (window.confirm('Are you sure?')) {
@@ -13,6 +19,8 @@ function TodoList() {
 
     return (
         <ul className="todo-list">
+            {isLoading && <Loading />}
+            {error && <Error message={error} />}
             {filteredItems.map((item) => (
                 <li key={item.id}>
                     <div className={item.completed ? "completed" : "view"}>
--- todoapp/src/components/redux/todos/todoSlice.js
+++ todoapp/src/components/redux/todos/todoSlice.js
@@ -1,21 +1,17 @@
-import { nanoid } from "@reduxjs/toolkit"
 
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from "axios"
+export const getTodosAsync = createAsyncThunk("todos/getTodosAsync", async () => {
+    const res = await axios("http://localhost:7000/todos");
+    return res.data
+});
+
 export const todosSlice = createSlice({
     name: "todos",
     initialState: {
-        items: [
-            {
-                id: "1",
-                title: "Learn JavaScript",
-                completed: true,
-            },
-            {
-                id: "2",
-                title: "Learn React",
-                completed: false,
-            },
-        ],
+        items: [],
+        isLoading: false,
+        error: null,
         activeFilter: "all",
     },
     reducers: {
@@ -54,6 +50,20 @@ export const todosSlice = createSlice({
             const filtered = state.items.filter(item => item.completed === false);
             state.items = filtered;
         }
+    },
+    extraReducers(builder) { // ExtraReducers kısmı Güncellenmiş versiyonudur
+        builder
+            .addCase(getTodosAsync.pending, (state) => {
+                state.isLoading = true; //Pending yani bekleme durumunda loadin true haline getiriyor
+            })
+            .addCase(getTodosAsync.fulfilled, (state, action) => {
+                state.items = action.payload; // FulFilled durumu olumlu durumunda items'a veri atıyor
+                state.isLoading = false; // loading false yapıyor
+            })
+            .addCase(getTodosAsync.rejected, (state, action) => {
+                state.isLoading = false; // rejected Durumunda loading false yapıyor
+                state.error = action.error.message; // rejected durumunda hata mesajı atıyor
+            })
     }
 })
 
