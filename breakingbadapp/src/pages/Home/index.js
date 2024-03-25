1ac01e44c539e979a70a17a0a4db2bbbf5f7dea3
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-25 14:06:58 +0300

    Çekilen karakterlerin listelenmesi

--- breakingbadapp/src/pages/Home/index.js
+++ breakingbadapp/src/pages/Home/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllCharacters } from '../../redux/services';
-
+import './styles.css';
+import Masonry from 'react-masonry-css';
+import Loading from '../../components/Loading';
+import Error from './../../components/Error';
 
 function Home() {
     const { items, isLoading, error } = useSelector(state => state.characters);
@@ -9,9 +12,28 @@ function Home() {
     useEffect(() => {
         dispatch(getAllCharacters())
     }, [dispatch])
-    console.log(items)
+    console.log(items.results)
+    console.log(items.info)
+
     return (
-        <div>Home</div>
+        <div>
+            <h1>Characters</h1>
+
+            {isLoading && <Loading />}
+            {error && <Error message={error} />}
+            <Masonry
+                breakpointCols={4}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column">
+                {items.results && items.results.map((character) =>
+                    <div key={character.id}>
+                        <img alt={character.name} src={character.image} className='character' />
+                        <div className='character_name'>{character.name}</div>
+                    </div>
+                )}
+            </Masonry>
+
+        </div>
     )
 }
 
--- breakingbadapp/src/redux/services.js
+++ breakingbadapp/src/redux/services.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_BASE_ENDPOINT;
 // Axios baseUrl configu edilir. Configu edilen baseUrl'den file istek atılır
-
+const page = 1; // Sayfa numarasını tut
 export const getAllCharacters = createAsyncThunk("characters/getAllCharacters", async () => {
-    const res = await axios(`/character`);
+    const res = await axios(`/character/?page=${page}`);
     return res.data
 });
